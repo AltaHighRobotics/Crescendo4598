293c670a9695db8b8772e0934410c61b0be7ccbd
Nathan <93161331+nathansmith117@users.noreply.github.com>
2024-01-11 16:16:28 -0700

    Wheels go spinny

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -50,13 +50,15 @@ public class DriveCommand extends Command {
     double speed = leftStickY;
     double rotation = rightStickX;
 
-    // m_driveTrainSub.drive(
-    //   Math.pow(strafe, 2.0) * Math.signum(strafe), 
-    //   -Math.pow(speed, 2.0) * Math.signum(speed), 
-    //   Math.pow(rotation, 2.0) * Math.signum(rotation) * Constants.DRIVE_TURN_SPEED,
-    //   true,
-    //   Constants.DRIVE_SPEED
-    // );
+    m_driveTrainSub.drive(
+      Math.pow(strafe, 2.0) * Math.signum(strafe), 
+      -Math.pow(speed, 2.0) * Math.signum(speed), 
+      Math.pow(rotation, 2.0) * Math.signum(rotation) * Constants.DRIVE_TURN_SPEED,
+      false,
+      Constants.DRIVE_SPEED
+    );
+
+    m_driveTrainSub.run();
   }
 
   // Called once the command ends or is interrupted.
