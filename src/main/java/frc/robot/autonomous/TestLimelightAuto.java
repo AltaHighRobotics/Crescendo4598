3539efb44b05a9cd53b2f8d28e36adb85ff8c7fb
Adam <114111495+Unicornflart@users.noreply.github.com>
2024-02-09 14:53:56 -0700

    Working on a limelight test auto

---
+++ src/main/java/frc/robot/autonomous/TestLimelightAuto.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.autonomous;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.swerve.DriveTrainSub;
+import frc.robot.subsystems.VisionSub;
+import frc.robot.swerve.AutoAlignment;
+import limelightvision.limelight.frc.LimeLight.LimeLightTransform;
+import utilities.CartesianVector;;
+
+public class TestLimelightAuto extends Command {
+  private DriveTrainSub m_driveTrainSub;
+  private VisionSub m_visionSub;
+  private AutoAlignment autoAlignment;
+
+  public TestLimelightAuto(DriveTrainSub driveTrainSub, VisionSub visionSub) {
+    m_driveTrainSub = driveTrainSub;
+    m_visionSub = visionSub;
+
+    addRequirements(m_driveTrainSub, m_visionSub);
+    // Use addRequirements() here to declare subsystem dependencies.
+
+    autoAlignment = new AutoAlignment(m_driveTrainSub);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_driveTrainSub.resetGyro();
+
+    LimeLightTransform transform = m_visionSub.getAprilTagPositionRobotRelative();
+    autoAlignment.start(new CartesianVector(transform.x, transform.y), 0.0);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    boolean done = autoAlignment.run();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
